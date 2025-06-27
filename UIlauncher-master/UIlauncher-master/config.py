259c133d512a7f9874b3e946eaaebe4fb445cd43
YshikageOne <clydeyu0725@gmail.com>
2025-06-27 23:12:54 +0800

    updated where it is now a web application

--- UIlauncher-master/UIlauncher-master/config.py
+++
@@ -1,1 +0,0 @@
-DB_PATH = 'LaundryItems.db'
--- UIlauncher-master/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-DB_PATH = 'LaundryItems.db'
-
-JWT_SECRET = 'supersecretkey'  # 🔐 Replace this in production
-JWT_ALGORITHM = 'HS256'
-JWT_EXP_DELTA_SECONDS = 20  # 20 secc token validity
